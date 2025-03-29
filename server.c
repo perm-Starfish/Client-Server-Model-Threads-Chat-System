1e1e86c123ed923a24fe29c03ad50e77f7d63cbe
Starfish <82247390+perm-Starfish@users.noreply.github.com>
2025-03-29 15:13:07 +0800

    Update server.c

--- server.c
+++ server.c
@@ -3,81 +3,163 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define MAX_CLIENTS 10
+#define MAX_NAME 50
+#define MAX_MSG_LEN 1024
 #define BUFFER_SIZE 1024
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-int client_sockets[MAX_CLIENTS];
-char *client_names[MAX_CLIENTS];
 
-void broadcast_message(char *message) {
+// define client information
+typedef struct {
+    int socket;
+    struct sockaddr_in address;
+    char name[MAX_NAME];
+    int active;
+} client_t;
+
+client_t clients[MAX_CLIENTS];  // list of clients
+
+void broadcast_message(const char *message) {
     pthread_mutex_lock(&mutex);
     for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] != 0) {
-            send(client_sockets[i], message, strlen(message), 0);
+        if (clients[i].active) {
+            send(clients[i].socket, message, strlen(message), 0);
         }
     }
     pthread_mutex_unlock(&mutex);
 }
 
 void *handle_client(void *arg) {
-    int client_socket = *(int *)arg;
+    client_t *client = (client_t *)arg;
     char buffer[BUFFER_SIZE];
-    char username[50];
-    recv(client_socket, username, sizeof(username), 0);
-    
-    pthread_mutex_lock(&mutex);
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] == 0) {
-            client_sockets[i] = client_socket;
-            client_names[i] = strdup(username);
-            break;
-        }
+    int bytes_received;
+
+    // client name
+    bytes_received = recv(client->socket, client->name, sizeof(client->name)-1, 0);
+    if (bytes_received <= 0) {
+        close(client->socket);
+        client->active = 0;
+        return NULL;
     }
-    pthread_mutex_unlock(&mutex);
-    
-    sprintf(buffer, "<User %s is on-line.>\n", username);
+    client->name[bytes_received] = '\0';
+
+    // broadcast new active user
+    sprintf(buffer, "<User %s is on-line, socket address: %s/%i>\n",
+            client->name, inet_ntoa(client->address.sin_addr), ntohs(client->address.sin_port));
     broadcast_message(buffer);
-    
+
     while (1) {
-        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+        bytes_received = recv(client->socket, buffer, sizeof(buffer)-1, 0);
         if (bytes_received <= 0) {
             break;
         }
         buffer[bytes_received] = '\0';
-        broadcast_message(buffer);
-    }
-    
-    close(client_socket);
-    pthread_mutex_lock(&mutex);
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] == client_socket) {
-            sprintf(buffer, "<User %s is off-line.>\n", client_names[i]);
-            free(client_names[i]);
-            client_sockets[i] = 0;
-            client_names[i] = NULL;
+
+        if (strncmp(buffer, "chat ", 5) == 0) {
+            char *message_start = strchr(buffer + 5, ' ');
+            if (message_start) {
+                *message_start = '\0';
+                char *recipient = buffer + 5;
+                char *message = message_start + 1;
+
+                pthread_mutex_lock(&mutex);
+                int found = 0;
+                for (int i = 0; i < MAX_CLIENTS; i++) {
+                    if (clients[i].active && strcmp(clients[i].name, recipient) == 0) {
+                        char private_msg[MAX_MSG_LEN];
+                        snprintf(private_msg, sizeof(private_msg), "<To %s> %s\n", client->name, message);
+                        send(clients[i].socket, private_msg, strlen(private_msg), 0);
+                        found = 1;
+                        break;
+                    }
+                }
+                pthread_mutex_unlock(&mutex);
+
+                if (!found) {
+                    send(client->socket, "User does not exist.\n", 16, 0);
+                }
+            }
+        } else if (strncmp(buffer, "bye", 3) == 0) {
             break;
+        } else {
+        	
         }
     }
+
+    // user off-line
+    close(client->socket);
+    pthread_mutex_lock(&mutex);
+    client->active = 0;
+    sprintf(buffer, "<User %s is off-line.>\n", client->name);
     pthread_mutex_unlock(&mutex);
-    
+
     broadcast_message(buffer);
     return NULL;
 }
 
 int main() {
+    struct sockaddr_in server, client;
+    socklen_t sockaddr_size = sizeof(struct sockaddr_in);
+
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(1234);
+    server.sin_addr.s_addr = INADDR_ANY;
+
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_addr = {AF_INET, htons(1234), INADDR_ANY};
-    bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    listen(server_socket, MAX_CLIENTS);
-    
-    printf("Server listening on port 1234...\n");
-    while (1) {
-        int client_socket = accept(server_socket, NULL, NULL);
-        pthread_t tid;
-        pthread_create(&tid, NULL, handle_client, &client_socket);
+    if (server_socket < 0) {
+        perror("Socket creation failed");
+        return 1;
     }
+
+    if (bind(server_socket, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        perror("Bind failed");
+        close(server_socket);
+        return 1;
+    }
+
+    if (listen(server_socket, MAX_CLIENTS) < 0) {
+        perror("Listen failed");
+        close(server_socket);
+        return 1;
+    }
+
+    printf("Server listening on port 1234...\n");
+
+    while (1) {
+        int new_socket = accept(server_socket, (struct sockaddr *)&client, &sockaddr_size);
+        if (new_socket < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        // find available client slot
+        pthread_mutex_lock(&mutex);
+        int i;
+        for (i = 0; i < MAX_CLIENTS; i++) {
+            if (!clients[i].active) {
+                clients[i].socket = new_socket;
+                clients[i].address = client;
+                clients[i].active = 1;
+                break;
+            }
+        }
+        pthread_mutex_unlock(&mutex);
+
+        if (i == MAX_CLIENTS) {
+            send(new_socket, "Server is full.\n", 16, 0);
+            close(new_socket);
+        } else {
+            pthread_t tid;
+            pthread_create(&tid, NULL, handle_client, &clients[i]);
+            pthread_detach(tid);
+        }
+    }
+
+    close(server_socket);
     return 0;
 }
